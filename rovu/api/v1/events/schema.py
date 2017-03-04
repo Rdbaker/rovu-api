1c79c042d88fa2f53d69f767e8cfcdc27730a31c
Ryan Baker <ryan.da.baker@gmail.com>
2017-03-04 11:24:56 -0500

    Adds categories to the API.

--- rovu/api/v1/events/schema.py
+++ rovu/api/v1/events/schema.py
@@ -6,6 +6,8 @@ from marshmallow import Schema, fields
 class EventSchema(Schema):
     id = fields.Integer(dump_only=True)
     name = fields.Str(attribute='eb_name_html')
+    # it's stored as a string internally, but let's convert it to an int
+    category_id = fields.Integer(attribute='eb_category_id')
     start_datetime = fields.DateTime()
     end_datetime = fields.DateTime()
     venue = fields.Raw(attribute='eb_venue')
@@ -19,3 +21,23 @@ class EventSchemaFull(EventSchema):
     description = fields.Str(attribute='eb_description_html')
     capacity = fields.Integer(attribute='eb_capacity')
     external_url = fields.URL(attribute='eb_url')
+
+
+class CategorySchema(Schema):
+    id = fields.Integer(dump_only=True)
+    # it's stored as a string internally, but let's convert it to an int
+    external_id = fields.Integer(attribute='eb_id')
+    parent_id = fields.String(attribute='eb_parent_id')
+    name = fields.Str(attribute='eb_name')
+    name_short = fields.Str(attribute='eb_name_short')
+
+    class Meta:
+        type_ = 'categories'
+        strict = True
+
+
+class CategoryFacetSchema(Schema):
+    # it's stored as a string internally, but let's convert it to an int
+    category_id = fields.Integer(attribute='eb_id')
+    event_count = fields.Integer()
+    name = fields.Str(attribute='eb_name')
--- rovu/api/v1/events/views.py
+++ rovu/api/v1/events/views.py
@@ -3,20 +3,25 @@
 from datetime import datetime
 from dateutil.parser import parse
 
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from sqlalchemy import func, desc
 
-from rovu.api.v1.events.models import Event
-from rovu.api.v1.events.schema import EventSchema
+from rovu.api.v1.events.models import Event, Category
+from rovu.api.v1.events.schema import (EventSchema, CategorySchema,
+                                       CategoryFacetSchema)
 
 blueprint = Blueprint('events', __name__, url_prefix='/api/v1/events')
 EVENT_SCHEMA = EventSchema()
+CAT_SCHEMA = CategorySchema()
+CF_SCHEMA = CategoryFacetSchema()
 
 
 @blueprint.route('/', strict_slashes=False)
 def events():
     """List events."""
-    event_start_after = request.args.get('event_start_after', None)
-    event_end_before = request.args.get('event_end_before', None)
+    event_start_after = request.args.get('event_start_after')
+    event_end_before = request.args.get('event_end_before')
+    category_id = request.args.get('category_id')
 
     if event_start_after is None:
         query_args = [Event.start_datetime >= datetime.utcnow().isoformat()]
@@ -26,5 +31,27 @@ def events():
     if event_end_before is not None:
         query_args.append(Event.end_datetime <= event_end_before)
 
-    return EVENT_SCHEMA.dumps(Event.query.filter(*query_args).all(),
-                              many=True)
+    if category_id is not None:
+        query_args.append(Event.eb_category_id == category_id)
+
+    return jsonify(EVENT_SCHEMA.dump(Event.query.filter(*query_args).all(),
+                                     many=True).data)
+
+
+@blueprint.route('/categories/all', strict_slashes=False)
+def categories():
+    """List all categories."""
+    return jsonify(CAT_SCHEMA.dump(Category.query.all(), many=True).data)
+
+
+@blueprint.route('/categories/facets', strict_slashes=False)
+def categories_facets():
+    """List facets of categories."""
+    query = Category.query.session.query(Category.eb_name,
+                                         Category.eb_id,
+                                         func.count('*').label('event_count'))\
+        .join(Event,
+              Category.eb_id == Event.eb_category_id)\
+        .group_by(Category.eb_name, Category.eb_id)\
+        .order_by(desc('event_count'))
+    return jsonify(CF_SCHEMA.dump(query.all(), many=True).data)
